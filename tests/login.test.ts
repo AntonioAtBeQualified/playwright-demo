90ed59fe8cdaaf7e4c5339471247ff3a6be16780
AntonioAtBeQualified <165880326+AntonioAtBeQualified@users.noreply.github.com>
2024-04-04 16:53:04 +0200

    fix logintest with beforeEach method

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -2,19 +2,33 @@ import {test, expect} from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 
 
-test("Open LoginPage and verify Title", async ({page}) => {
-    const loginPage = new LoginPage(page);
-    loginPage.navigate();
-    await expect(page).toHaveTitle('Swag Labs');
+test.describe ('navigate to login', () => {
+    
+    // this test navigates always to the LoginPage for every Test
+    test.beforeEach( async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate()
+    })
+    test("Open LoginPage and verify Title", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
+        await expect(page).toHaveTitle('Swag Labs');
+    })
+    
+    test("login with wrong credentials", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
+        await loginPage.checkerror();
+    })
+    
+    test("Verify Login is Possible", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
+        await loginPage.login();
+    })
 })
 
-test("Should show error message", async ({page}) => {
-    const loginPage = new LoginPage(page);
-    loginPage.checkerror();
-})
 
 
-test("Verify Login is Possible", async ({page}) => {
-    const loginPage = new LoginPage(page);
-    loginPage.login();
-})
+/*test("login with wrong credentials", async )
+test("login with admin")*/
