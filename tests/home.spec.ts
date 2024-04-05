7b9fa571fff849845de6200e4194112f62f8f6e5
AntonioAtBeQualified <165880326+AntonioAtBeQualified@users.noreply.github.com>
2024-04-05 10:27:44 +0200

    Delete initial test from Project

--- tests/home.spec.ts
+++
@@ -1,8 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-
-test("Open home Page and verify UsernameField", async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-
-    await expect(page).toHaveTitle('Swag Labs');
-})
